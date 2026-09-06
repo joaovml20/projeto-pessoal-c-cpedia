080e74af3a4f9fc1dcddcb3d0bf1dae5a4412c70
joaovml20 <joaolima15257@gmail.com>
2026-09-06 20:38:22 -0300

    alocacao dinamica, atualizando informacoes sobre malloc e adicao de calloc

--- alocacao_dinamica/calloc.c
+++ alocacao_dinamica/calloc.c
@@ -1,10 +1,10 @@
 // Alocação dinâmica em c nos permitir alocar memoria em tempo de execuçâo, por exemplo, uma variavel de determinado tamanho pode ser modificada para ter mais ou menos tamanho, diferente da alocação estática, onde a variavel teria um tamanho fixo do inicio ao fim do programar quando declarada. com a diferença tambem que a dinâmica utilizar e precisar de ponteiros, diferente da estática que é opcional. para esse tipo de alocação, sera usado um vetor de exemplo
-// um dos metodos para utilizar a alocação dinâmica é o malloc
-// o malloc no modo utilizado aqui, recebe o numero em bytes e retorna para a variavel, a variavel assumir aquele numero de bytes como seu espaço para armazenar dados
+// um dos metodos para utilizar a alocação dinâmica é o calloc
+// o calloc no modo utilizado aqui, recebera dois parametros, o primeiro é o tamanho da variavel, e o segundo o tamanho do tipo da variavel com sizeof(). sizeof() é uma outra função que retornara o tamanho de determinado tipo de dado em bytes, assim não sera necessario o conhecimento do tamanho do tipo de dado pos sizeof() ja faz esses trabalho. logo apos esse processo, a variavel que recebe calloc tera o tamanho correspondente ao primeiro parametro e ja prrenchido com valores dependendo do tipo da variavel, pode ser desde 0 se for int ou ate uma string vazia se for char
 // importe resaltar que para ele funcionar, é necessario informar o tipo do ponteiro antes dele com parenteses e com isso, ser passado como valor para a variavel ponteiro do tipo informado, se caso der errado, ele retornara NULL
 // um exemplo abaixo
 #include <stdio.h>
-#include <stdlib.h> // biblioteca para utilização da função malloc
+#include <stdlib.h> // biblioteca para utilização da função calloc
 
 int main(){
     int tamanho,*vetor;
@@ -12,19 +12,17 @@ int main(){
 
     printf("Digite o tamanho do vetor: ");
     scanf("%d",&tamanho);
-    vetor = (int*) malloc(tamanho * sizeof(int));
-    // acima o processo com malloc, como dito acima, ele recebe o tamanho em bytes, nesse caso, ele recebe o tamanho informado pelo usuario e multiplicar com sizeof(int), sizeof() é uma outra função que retornara nesse caso o tamanho de bytes do tipo int definido pelo SO, então considerado que o tamanho de bytes do tipo int é 4 e o valor do tamanho informado pelo usuario seja 10, o resultado sera 40, logo esse 40 que é 40 bytes é o tamanho da variavel vetor se o processo com malloc dê certo
-    // após esse calculo, o resultado será convertido adequadamente pelo tipo da variavel com asteristico, nesse caso, como a variavel vetor é do tipo inteiro, sera int* entre parenteses como visto na linha, (int*)
-    // por fim, se caso deu tudo certo, agora a variavel ponteiro(vetor) tem o tamanho definido pela variavel tamanho, ou seja, se caso o usuario tenha informado o valor da variavel tamanho como 10, logo, vetor tem 10 de espaço para armazenar 10 numeros do tipo int. agora se caso não deu certo, vetor estara com o valor NUll, isso ocorre quando não a memoria o suficiente ou algum erro de logica ou outros motivos
+    vetor = (int*) calloc(tamanho,sizeof(int));
+    // acima o processo com calloc, como dito nos comentarios acima, ele recebe o tamanho que tera o vetor que nesse caso o usuario ira informar, e o segundo parametro é o sizeof() passando o tipo da variavel ponteiro, para que assim, ele retorne corretamente o tamanho do tipo de dados para a função. apos esse processo, a esquerda de calloc estara novamente o tipo de dado dessa vez com asteristico e entre aspas(int*) para garantir a conversão que enfim sera atribuido a variavel vetor para ter seu tamanho de memoria alocado dinamicamente
+    // se o processo com calloc tenha dado erro seja por falta de memoria ou erro de logica ou outros motivos, a variavel vetor estara com valor NULL
     //abaixo, o tratamento se caso a alocação dinâmica com vetor não tenha dado certo, isso é extremamente importante para que o programar não prossiga com a variavel cujo não processou corretamente como queriamos
     if(vetor == NULL){
         printf("Erro na alocação Dinâmica");
         return 1; // return 1, de modo simples, significar que o programar encerrara com um erro
     }
 
-    // agora com um vetor alocado dinamicamente, ele esta vazio, sera preenchido seus espaços com um laço, para exemplo
+    // agora com um vetor alocado dinamicamente, sera mostrado o que ele contem, lembre-se que calloc sempre preenche os espaços com algum dado dependendo do tipo da variavel, desde 0 para int ou um espaço vazio para char
     for(int i = 0;i <= tamanho; i++){ 
-        vetor[i] = i;
         printf("Resultado: %d\n",vetor[i]);
     }
 
